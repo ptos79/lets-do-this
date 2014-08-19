3fecf2be3948fcb415a88f3950723a25cedb30df
Patrick O'Shea <patrick.oshea@wavebreakmedia.com>
2014-08-19 13:07:47 +0100

    test comments

--- apiSettings.js
+++ apiSettings.js
@@ -9,7 +9,7 @@
 
 var fs = require('fs');
 var _ = require('underscore');
-
+//this is a comment
 module.exports.apiSettings = {
     descriptors: loadDescriptors(),
     swaggerUI: loadSwaggerUI(),
